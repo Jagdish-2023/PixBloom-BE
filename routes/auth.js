07056348ca924613c802162a7aa4062e20b946d6
Jagdish-2023 <jagdishjpradhan@gmail.com>
2025-07-01 14:55:13 +0530

    Chore: Added guest login

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,27 @@ const PORT = process.env.PORT;
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const JWT_SECRET = process.env.JWT_SECRET;
+const GUEST_EMAIL = process.env.GUEST_EMAIL;
+
+router.get("/login/guest", async (req, res) => {
+  try {
+    const findGuest = await User.findOne({ email: GUEST_EMAIL });
+    if (findGuest) {
+      const token = jwt.sign({ id: findGuest._id, role: "guest" }, JWT_SECRET, {
+        expiresIn: "24h",
+      });
+
+      res.cookie("accessToken", token, {
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+      });
+    }
+    res.redirect(`${process.env.FRONTEND_URL}/photos`);
+  } catch (error) {
+    console.error(error);
+    res.status(500)._construct.send("Something went wrong. Please try again");
+  }
+});
 
 router.get("/google", async (req, res) => {
   const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
